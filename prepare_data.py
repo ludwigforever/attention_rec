7ab304b7e6d60979ecabb90571cec5f6ede3b614
ludwigforever <ludwigforever@163.com>
2019-03-21 08:48:00 +0800

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -58,6 +58,12 @@ class DataHandler(object):
         one_hot_encoding[movie_id] = 1
         return np.concatenate((one_hot_encoding, self.tag2code(genres)))
 
+    def get_features_lstm(self, user_id, movie_in):
+        movie_id, genres = movie_in[0], movie_in[1]
+        one_hot_encoding = np.zeros(self.n_movies)
+        one_hot_encoding[movie_id] = 1
+        return np.concatenate((self.users_code[int(user_id)], one_hot_encoding, self.tag2code(genres)))
+
     def prepare_input(self, sequences):
 
         X = np.zeros((self.batch_size, self.max_length, self.n_movies + self.n_genres), dtype='float')
@@ -93,6 +99,24 @@ class DataHandler(object):
         new = np.concatenate((U, X), axis=2)
         return (new, Y)
 
+    def prepare_input_with_user_lstm(self, sequences):
+        X = np.zeros((self.batch_size, self.max_length, self.n_movies + self.n_genres + self.n_usercode), dtype='float')
+        Y = np.zeros((self.batch_size, self.n_movies), dtype='float')
+        for i, sequence in enumerate(sequences):
+            user_id, in_seq, target = sequence
+            '''print('prepare_input:user_id')
+            print(user_id)
+            print('prepare_input:in_seq')
+            print(in_seq)
+            print('prepare_input:target')
+            print(target)'''
+            seq_features = np.array(list(map(lambda x: self.get_features_lstm(user_id, x), in_seq)))
+            X[i, self.max_length - len(in_seq):, :] = seq_features
+            one_hot_encoding = np.zeros(self.n_movies)
+            one_hot_encoding[target[0]] = 1
+            Y[i] = one_hot_encoding
+        return (X, Y)
+
     def gen_mini_batch(self, sequence_generator, test=False, max_reuse_sequence=np.inf):
         batch_size = self.batch_size
         while True:
@@ -127,12 +151,54 @@ class DataHandler(object):
             else:
                 yield self.prepare_input_with_user(sequences)
 
+    def gen_mini_batch_lstm(self, sequence_generator, test=False, max_reuse_sequence=np.inf):
+        batch_size = self.batch_size
+        while True:
+            j = 0
+            sequences = []
+
+            if test:
+                batch_size = 1
+            while j < batch_size:
+
+                sequence, user_id = next(sequence_generator)
+                # finds the lengths of the different subsequences
+                if not test:
+                    # print('next')
+                    # print(sequence)
+                    seq_lengths = sorted(random.sample(range(5, len(sequence)),
+                                                       min([batch_size - j,
+                                                            len(sequence) - 2,
+                                                            max_reuse_sequence])))
+                else:
+                    seq_lengths = [int(len(sequence) / 2)]
+
+                skipped_seq = 0
+                for l in seq_lengths:
+                    start = max(0, l - self.max_length)  # sequences cannot be longer than self.max_lenght
+                    target = sequence[l]
+                    sequences.append([user_id, sequence[start:l], target])
+                j += len(seq_lengths)
+
+            if test:
+                yield self.prepare_input_with_user_lstm(sequences), [i[0] for i in sequence[seq_lengths[0]:]]
+            else:
+                yield self.prepare_input_with_user_lstm(sequences)
+
     def get_train_data(self):
         train_data, n_train_user = self.load_data('./data/train_set_sequences(extend)')
         val_data, n_val_user = self.load_data('./data/val_set_sequences(extend)')
         training_set = self.gen_mini_batch(self.sequence_generator(train_data))
         validation_set = self.gen_mini_batch(self.sequence_generator(val_data))
         return training_set, validation_set, n_train_user, n_val_user
+
+    def get_train_data_lstm(self):
+        train_data, n_train_user = self.load_data('./data/train_set_sequences(extend)')
+        val_data, n_val_user = self.load_data('./data/val_set_sequences(extend)')
+        training_set = self.gen_mini_batch_lstm(self.sequence_generator(train_data))
+        validation_set = self.gen_mini_batch_lstm(self.sequence_generator(val_data))
+        return training_set, validation_set, n_train_user, n_val_user
+
 
     def get_user_data(self):
         user_data, n_users = self.load_data('./data/user_set')
