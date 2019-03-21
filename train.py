fad612a59e0094b3fcd0a2a3f335f81b12069fe7
ludwigforever <ludwigforever@163.com>
2019-03-21 17:37:44 +0800

    Update train.py

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def main():
 
     start_time = time.time()
 
-    filepath = "model_extend/model_{epoch:02d}-{val_top_10_CCE:.7f}.h5"
+    filepath = "model/model_{epoch:02d}-{val_top_10_CCE:.7f}.h5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_top_10_CCE', verbose=1, mode='max')
     reduce_lr_on_pl = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1,
                                                         mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
