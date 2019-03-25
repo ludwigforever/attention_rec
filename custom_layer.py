b524510b7ea9e8b50d6724ac2a383e5c6c4e4d6e
ludwigforever <ludwigforever@163.com>
2019-03-25 20:15:02 +0800

    Update custom_layer.py

--- custom_layer.py
+++ custom_layer.py
@@ -598,3 +598,152 @@ class similar_RNN_multi(Layer):
       
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.units)
+
+class genres_similar(Layer):
+
+    def __init__(self, units, dropout=0., **kwargs):
+        self.units = units # 输出维度
+        self.dropout = min(1., max(0., dropout))
+        self.supports_masking = True
+        super(genres_similar, self).__init__(**kwargs)
+    
+    def compute_mask(self, inputs, mask):
+        if isinstance(mask, list):
+            mask = mask[0]
+        output_mask =  None
+        return output_mask
+    
+    def build(self, input_shape): # 定义可训练参数
+        self.encode_kernel = self.add_weight(name='encode_kernel',
+                                      shape=(input_shape[-1], self.units),
+                                      initializer='glorot_normal',
+                                      trainable=True)
+        
+        self.query_kernel = self.add_weight(name='query_kernel',
+                                      shape=(self.units*2, self.units*4),
+                                      initializer='glorot_normal',
+                                      trainable=True)
+        
+        self.key_kernel = self.add_weight(name='key_kernel',
+                                      shape=(self.units*2, self.units*4),
+                                      initializer='glorot_normal',
+                                      trainable=True)
+        
+        self.value_kernel = self.add_weight(name='value_kernel',
+                                      shape=(self.units*2, self.units*4),
+                                      initializer='glorot_normal',
+                                      trainable=True)
+        
+        self.switch_kernel = self.add_weight(name='switch_kernel',
+                                      shape=(self.units*4, self.units),
+                                      initializer='glorot_normal',
+                                      trainable=True)
+        #print('input_shape',input_shape)
+        
+        self.query_kernel1 = self.query_kernel[:, :self.units]
+        self.query_kernel2 = self.query_kernel[:, self.units:self.units*2]
+        self.query_kernel3 = self.query_kernel[:, self.units*2:self.units*3]
+        self.query_kernel4 = self.query_kernel[:, self.units*3:]
+        
+        self.key_kernel1 = self.key_kernel[:, :self.units]
+        self.key_kernel2 = self.key_kernel[:, self.units:self.units*2]
+        self.key_kernel3 = self.key_kernel[:, self.units*2:self.units*3]
+        self.key_kernel4 = self.key_kernel[:, self.units*3:]
+        
+        self.value_kernel1 = self.value_kernel[:, :self.units]
+        self.value_kernel2 = self.value_kernel[:, self.units:self.units*2]
+        self.value_kernel3 = self.value_kernel[:, self.units*2:self.units*3]
+        self.value_kernel4 = self.value_kernel[:, self.units*3:]
+        
+
+    def step_do(self, step_in, states): # 定义每一步的迭代
+        
+        in_value = step_in
+        if 0 < self.dropout < 1.:
+            self._dropout_mask = K.in_train_phase(K.dropout(K.ones_like(step_in), self.dropout), K.ones_like(step_in))
+        if 0 < self.dropout < 1.:
+            in_value = step_in * self._dropout_mask
+            
+        g1 = states[0][:, :-18]
+        g2 = states[1][:, :-18]
+        g3 = in_value[:, :-18]
+        
+        d1 = K.sigmoid(K.sqrt(K.sum((K.square(g3-g1)),axis=-1,keepdims=True)))
+        d2 = K.sigmoid(K.sqrt(K.sum((K.square(g3-g2)),axis=-1,keepdims=True)))
+        '''
+        d1 = K.sigmoid(K.sum((K.abs(in_value-states[0])/self.units),axis=-1,keepdims=True))
+        d2 = K.sigmoid(K.sum((K.abs(in_value-states[1])/self.units),axis=-1,keepdims=True))
+        '''
+        #print('d1.shape',d1.shape)
+        state1 = d1*states[0] + (1-d1)*in_value
+        print('state1.shape',state1.shape)
+        state2 = (1-d2)*states[0] + d2*in_value
+        '''
+        lt = K.expand_dims(state1,axis=-2)
+        st = K.expand_dims(state2,axis=-2)
+        outputs = K.concatenate([lt, st], axis=-2)
+        '''
+        out1 = K.dot(state1, self.encode_kernel)
+        out2 = K.dot(state2, self.encode_kernel)
+        
+        outputs = K.concatenate([out1, out2], axis=-1)
+        outputs = K.relu(outputs)
+        
+        return outputs, [state1, state2]
+    
+    def call(self, inputs): # 定义正式执行的函数
+        
+        init_states = [K.zeros((K.shape(inputs)[0],K.shape(inputs)[-1])), K.zeros((K.shape(inputs)[0],K.shape(inputs)[-1]))] # 定义初始态(全零)
+        #init_states = [inputs[:,0], inputs[:,0]]
+        #print('inputs',K.shape(inputs)[0])
+        outputs = K.rnn(self.step_do, inputs, init_states, unroll=False) # 循环执行step_do函数
+        #print('outputs[1]',outputs.shape)
+        
+        print('outputs[0].shape',outputs[0].shape)
+        
+        query1=K.dot(outputs[1], self.query_kernel1)
+        
+        key1=K.dot(outputs[1], self.key_kernel1)
+        
+        value1=K.dot(outputs[1], self.value_kernel1)
+        
+        attention_prob1 = K.batch_dot(query1, key1, axes=[2, 2])/np.sqrt(self.units)
+        attention_prob1 = K.softmax(attention_prob1)
+        att_out1 = K.batch_dot(attention_prob1, value1, axes=[2, 1])
+        
+        query2=K.dot(outputs[1], self.query_kernel2)
+        
+        key2=K.dot(outputs[1], self.key_kernel2)
+        
+        value2=K.dot(outputs[1], self.value_kernel2)
+        
+        attention_prob2 = K.batch_dot(query2, key2, axes=[2, 2])/np.sqrt(self.units)
+        attention_prob2 = K.softmax(attention_prob2)
+        att_out2 = K.batch_dot(attention_prob2, value2, axes=[2, 1])
+        
+        query3=K.dot(outputs[1], self.query_kernel3)
+        
+        key3=K.dot(outputs[1], self.key_kernel3)
+        
+        value3=K.dot(outputs[1], self.value_kernel3)
+        
+        attention_prob3 = K.batch_dot(query3, key3, axes=[2, 2])/np.sqrt(self.units)
+        attention_prob3 = K.softmax(attention_prob3)
+        att_out3 = K.batch_dot(attention_prob3, value3, axes=[2, 1])
+        
+        query4=K.dot(outputs[1], self.query_kernel4)
+        
+        key4=K.dot(outputs[1], self.key_kernel4)
+        
+        value4=K.dot(outputs[1], self.value_kernel4)
+        
+        attention_prob4 = K.batch_dot(query4, key4, axes=[2, 2])/np.sqrt(self.units)
+        attention_prob4 = K.softmax(attention_prob4)
+        att_out4 = K.batch_dot(attention_prob4, value4, axes=[2, 1])
+        
+        att_out = K.concatenate([att_out1, att_out2, att_out3, att_out4], axis=-1)
+        out = K.dot(att_out, self.switch_kernel)
+        return out[:, -1]
+      
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.units)
