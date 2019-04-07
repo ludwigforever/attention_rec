50f4b4f971182bf81be3eb84fa76c2a2d2405cf1
ludwigforever <ludwigforever@163.com>
2019-04-07 16:07:57 +0800

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -73,7 +73,8 @@ class DataHandler(object):
             seq_features = np.array(list(map(lambda x: self.get_features(x), in_seq)))
             X[i, self.max_length - len(in_seq):, :] = seq_features
             one_hot_encoding = np.zeros(self.n_movies)
-            #one_hot_encoding[target[0]] = 1
+            one_hot_encoding[target[0]] = 1
+            '''
             one_hot_encoding[target[0][0]] = 1
             one_hot_encoding[target[1][0]] = 1
             one_hot_encoding[target[2][0]] = 1
@@ -84,6 +85,7 @@ class DataHandler(object):
             one_hot_encoding[target[7][0]] = 1
             one_hot_encoding[target[8][0]] = 1
             one_hot_encoding[target[9][0]] = 1
+            '''
             Y[i] = one_hot_encoding
         return (X, Y)
 
@@ -159,7 +161,7 @@ class DataHandler(object):
                 if not test:
                     # print('next')
                     # print(sequence)
-                    seq_lengths = sorted(random.sample(range(2, len(sequence)-9),
+                    seq_lengths = sorted(random.sample(range(5, len(sequence)),
                                                        min([batch_size - j,
                                                             len(sequence) - 2,
                                                             max_reuse_sequence])))
